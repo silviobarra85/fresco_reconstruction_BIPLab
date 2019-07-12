ca8333ea047379d2b00938be387f933f541a78a3
Silvio Barra <barra.silvio@gmail.com>
2019-07-12 14:19:54 +0200

    Bug Fixed
    Bug in the reading of folders arguments

--- src/dafne.py
+++ src/dafne.py
@@ -23,15 +23,15 @@ def coeff_angolare(x1, y1, x2 , y2):
 	return numeratore/denominatore
 
 def distanza(x1, y1, x2, y2): 
-	d12 = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+	d12 = math.sqrt((x1-x2)**2 + (y1-y2)**2) #Euclidean Distance
 	return d12
 
 def ricomponi(frammento , immagine_intera, output):
 	whole_image = immagine_intera
 	img1_filename = frammento 
 
-	img1 = cv2.imread(img1_filename, 0) # queryImage
-	img2 = cv2.imread(whole_image, 0) # trainImage
+	img1 = cv2.imread(img1_filename, 0) # fragments
+	img2 = cv2.imread(whole_image, 0) # whole image
 
 	img1_alfa = cv2.imread(img1_filename, cv2.IMREAD_UNCHANGED) # trainImage CON ALPHA CHANNEL
 
@@ -39,20 +39,20 @@ def ricomponi(frammento , immagine_intera, output):
 	    print('Could not open or find the images!')
 	    exit(0)
 
-	minHessian = 400
-	surf = cv2.xfeatures2d_SIFT.create()
+	#minHessian = 400
+	sift = cv2.xfeatures2d_SIFT.create()
 
-	surf_kp1, surf_des1 = surf.detectAndCompute(img1,None)
-	surf_kp2, surf_des2 = surf.detectAndCompute(img2,None)
+	sift_kp1, sift_des1 = sift.detectAndCompute(img1,None)
+	sift_kp2, sift_des2 = sift.detectAndCompute(img2,None)
 
 	matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
-	knn_matches = matcher.knnMatch(surf_des1, surf_des2, 2)
+	knn_matches = matcher.knnMatch(sift_des1, sift_des2, 2)
 
 	ratio_thresh = 0.7
 	good_matches = []
 
-	idx1 = []	# gli indici dei kp nell'immagine piccola
-	idx2 = []		# gli indici dei kp nell'immagine grande
+	idx1 = []	# keypoints indexes in the fragment
+	idx2 = []	# keypoints indexes in the big image
 
 	indice_kp = 0 
 	for m,n in knn_matches:
@@ -74,7 +74,7 @@ def ricomponi(frammento , immagine_intera, output):
 		origine = punto1.trainIdx
 		for punto2 in good_matches:
 			destino = punto2.trainIdx
-			dist_mom = distanza (surf_kp2[origine].pt[0] ,surf_kp2[origine].pt[1], surf_kp2[destino].pt[0], surf_kp2[destino].pt[1])
+			dist_mom = distanza (sift_kp2[origine].pt[0] ,sift_kp2[origine].pt[1], sift_kp2[destino].pt[0], sift_kp2[destino].pt[1])
 			if(dist_mom != 0 and dist_mom < dist_minima):
 				dist_minima = dist_mom
 				origine_big = origine
@@ -88,16 +88,16 @@ def ricomponi(frammento , immagine_intera, output):
 
 	#----- SCALE 
 
-	big_1x = surf_kp2[origine_big].pt[0]
-	big_1y = surf_kp2[origine_big].pt[1]
-	big_2x = surf_kp2[destino_big].pt[0]
-	big_2y = surf_kp2[destino_big].pt[1]
+	big_1x = sift_kp2[origine_big].pt[0]
+	big_1y = sift_kp2[origine_big].pt[1]
+	big_2x = sift_kp2[destino_big].pt[0]
+	big_2y = sift_kp2[destino_big].pt[1]
 
 	d_big = distanza(big_1x,big_1y, big_2x, big_2y)
 
 	# DISTANZA S1 - S2  - little image
-	little_1 = surf_kp1[idx1[idx_origine_little]]
-	little_2 = surf_kp1[idx1[idx_destino_little]]
+	little_1 = sift_kp1[idx1[idx_origine_little]]
+	little_2 = sift_kp1[idx1[idx_destino_little]]
 
 	little_1x = little_1.pt[0]
 	little_1y = little_1.pt[1]
@@ -247,27 +247,25 @@ def ricomponi(frammento , immagine_intera, output):
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-d', '--dbfld', dest='data_folder', type=str, default='../data/DAFNE_DB2/',
                         help='The dataset folder.')
-parser.add_argument('-l','--list', nargs='+', help='<Required> Set flag', required=True)
+parser.add_argument('-l','--list', dest="fresco_folders", nargs='+', help='<Required> Set flag', required=True)
 parser.add_argument('-o', '--outfolder', dest='output_folder', type=str, default='../SOLUTION/')
 
 args = parser.parse_args()
 
-output_folder = args.output_folder;
+
 data_folder = args.data_folder;
-folder = args._get_kwargs();
+fresco_folders = args.fresco_folders;
+output_folder = args.output_folder;
 try:
 	os.mkdir(output_folder)
 except:
 	print("Folder already exists")
 
-#data_folder = "../data/DAFNE_DB2/"
-#folder = ["01_Domenichino_Virgin-and-unicorn", "02_Zuccari_Cardinal-hat", "03_Andrea-di-Bonaiuto_Via-Veritas"]
 testo = ""
 
 
-for fresco_fld in os.listdir(data_folder):
+for fresco_fld in fresco_folders:
 	if not (fresco_fld.startswith('.')):#in case you are working on OsX, this avoids to include .DS_Store
-		print(fresco_fld)
 
 		img_number = int(fresco_fld[0:2])
 		img_name = fresco_fld[3:] #cut the number in front of the folder name
