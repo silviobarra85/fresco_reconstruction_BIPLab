d79505e06c1e98cb1b9e99e1f4dc4c295980ad10
Paps90 <barra90@gmail.com>
2019-07-12 15:03:15 +0200

    update dafne.py
    rotation criteria modded

--- src/dafne.py
+++ src/dafne.py
@@ -23,15 +23,16 @@ def coeff_angolare(x1, y1, x2 , y2):
 	return numeratore/denominatore
 
 def distanza(x1, y1, x2, y2): 
-	d12 = math.sqrt((x1-x2)**2 + (y1-y2)**2) #Euclidean Distance
+	d12 = math.sqrt((x1-x2)**2 + (y1-y2)**2)
 	return d12
+
 
 def ricomponi(frammento , immagine_intera, output):
 	whole_image = immagine_intera
 	img1_filename = frammento 
 
-	img1 = cv2.imread(img1_filename, 0) # fragments
-	img2 = cv2.imread(whole_image, 0) # whole image
+	img1 = cv2.imread(img1_filename, 0)          # queryImage 
+	img2 = cv2.imread(whole_image, 0) # trainImage
 
 	img1_alfa = cv2.imread(img1_filename, cv2.IMREAD_UNCHANGED) # trainImage CON ALPHA CHANNEL
 
@@ -39,20 +40,21 @@ def ricomponi(frammento , immagine_intera, output):
 	    print('Could not open or find the images!')
 	    exit(0)
 
-	#minHessian = 400
-	sift = cv2.xfeatures2d_SIFT.create()
+	minHessian = 400
+	surf = cv2.xfeatures2d_SIFT.create()
 
-	sift_kp1, sift_des1 = sift.detectAndCompute(img1,None)
-	sift_kp2, sift_des2 = sift.detectAndCompute(img2,None)
+	surf_kp1, surf_des1 = surf.detectAndCompute(img1,None)
+	surf_kp2, surf_des2 = surf.detectAndCompute(img2,None)
 
 	matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
-	knn_matches = matcher.knnMatch(sift_des1, sift_des2, 2)
+	
+	knn_matches = matcher.knnMatch(surf_des1, surf_des2, 2)
 
 	ratio_thresh = 0.7
 	good_matches = []
 
-	idx1 = []	# keypoints indexes in the fragment
-	idx2 = []	# keypoints indexes in the big image
+	idx1 = []	# gli indici dei kp nell'immagine piccola
+	idx2 = []		# gli indici dei kp nell'immagine grande
 
 	indice_kp = 0 
 	for m,n in knn_matches:
@@ -70,13 +72,39 @@ def ricomponi(frammento , immagine_intera, output):
 	indice_origine = indice_destino = 0 
 	idx_origine_little = idx_destino_little = 0 
 
+
+#	for punto1 in good_matches:
+#		origine = punto1.trainIdx
+#		for punto2 in good_matches:
+#			destino = punto2.trainIdx
+#			dist_mom = distanza (surf_kp2[origine].pt[0] ,surf_kp2[origine].pt[1], surf_kp2[destino].pt[0], surf_kp2[destino].pt[1])
+#			if(dist_mom != 0 and dist_mom < dist_minima):
+#				dist_minima = dist_mom
+#				origine_big = origine
+#				destino_big = destino
+#				idx_origine_little = indice_origine
+#				idx_destino_little = indice_destino
+#			indice_destino = indice_destino + 1 
+#		indice_origine = indice_origine + 1 
+#		indice_destino  = 0 
+
+
+
 	for punto1 in good_matches:
 		origine = punto1.trainIdx
 		for punto2 in good_matches:
 			destino = punto2.trainIdx
-			dist_mom = distanza (sift_kp2[origine].pt[0] ,sift_kp2[origine].pt[1], sift_kp2[destino].pt[0], sift_kp2[destino].pt[1])
-			if(dist_mom != 0 and dist_mom < dist_minima):
-				dist_minima = dist_mom
+			dist_2 = distanza (surf_kp2[origine].pt[0] ,surf_kp2[origine].pt[1], surf_kp2[destino].pt[0], surf_kp2[destino].pt[1])
+			dist_1 = distanza (surf_kp1[idx1[indice_origine]].pt[0] ,surf_kp1[idx1[indice_origine]].pt[1], surf_kp1[idx1[indice_destino]].pt[0], surf_kp1[idx1[indice_destino]].pt[0])
+			
+			dist_1 = int(dist_1)
+			dist_2 = int(dist_2)
+
+			#print(dist_1 , "  -- distanze --  " , dist_2)
+			if(dist_1 != 0 and dist_2 != 0 and dist_1 == dist_2):
+				print(dist_1 , "  -- distanze --  " , dist_2)
+				print("------------------ ENTER -----------------------")
+				#dist_minima = dist_mom
 				origine_big = origine
 				destino_big = destino
 				idx_origine_little = indice_origine
@@ -88,16 +116,16 @@ def ricomponi(frammento , immagine_intera, output):
 
 	#----- SCALE 
 
-	big_1x = sift_kp2[origine_big].pt[0]
-	big_1y = sift_kp2[origine_big].pt[1]
-	big_2x = sift_kp2[destino_big].pt[0]
-	big_2y = sift_kp2[destino_big].pt[1]
+	big_1x = surf_kp2[origine_big].pt[0]
+	big_1y = surf_kp2[origine_big].pt[1]
+	big_2x = surf_kp2[destino_big].pt[0]
+	big_2y = surf_kp2[destino_big].pt[1]
 
 	d_big = distanza(big_1x,big_1y, big_2x, big_2y)
 
 	# DISTANZA S1 - S2  - little image
-	little_1 = sift_kp1[idx1[idx_origine_little]]
-	little_2 = sift_kp1[idx1[idx_destino_little]]
+	little_1 = surf_kp1[idx1[idx_origine_little]]
+	little_2 = surf_kp1[idx1[idx_destino_little]]
 
 	little_1x = little_1.pt[0]
 	little_1y = little_1.pt[1]
@@ -106,16 +134,6 @@ def ricomponi(frammento , immagine_intera, output):
 
 	d_little = distanza(little_1x, little_1y, little_2x, little_2y)
 
-	altezza = img1.shape[0] 
-	larghezza = img1.shape[1]
-
-	altezza_new = int((d_big * altezza)/d_little)
-
-	larghezza_new = int((d_big * larghezza)/d_little)
-
-	#nuova_immagine = cv2.resize(img1, (altezza_new , larghezza_new))
-
-	#img1_alfa = cv2.resize(img1_alfa, (altezza_new , larghezza_new))
 
 	nuova_immagine = img1
 
@@ -240,27 +258,6 @@ def ricomponi(frammento , immagine_intera, output):
 	return coordinata_x, coordinata_y, angolo_di_rotazione
 
 	
-
-
-
-
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('-d', '--dbfld', dest='data_folder', type=str, default='../data/DAFNE_DB2/',
-                        help='The dataset folder.')
-parser.add_argument('-l','--list', dest="fresco_folders", nargs='+', help='<Required> Set flag', required=True)
-parser.add_argument('-o', '--outfolder', dest='output_folder', type=str, default='../SOLUTION/')
-
-args = parser.parse_args()
-
-
-data_folder = args.data_folder;
-fresco_folders = args.fresco_folders;
-output_folder = args.output_folder;
-try:
-	os.mkdir(output_folder)
-except:
-	print("Folder already exists")
-
 testo = ""
 
 
